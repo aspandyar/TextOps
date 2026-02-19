aefb77d319e000a6c18719e835b47356d0b96fba
aspandyar <aspandyart@gmail.com>
2026-02-19 07:42:22 +0500

    clean up

--- src/__tests__/mocks/browser.js
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from 'msw/browser';
-import { handlers } from './handlers.js';
-
-// Setup MSW worker for browser (development)
-export const worker = setupWorker(...handlers);
--- src/__tests__/mocks/server.js
+++
@@ -1,8 +0,0 @@
-import { setupServer } from 'msw/node';
-import { handlers, errorHandlers } from './handlers.js';
-
-// Setup MSW server for Node.js (testing environment)
-export const server = setupServer(...handlers);
-
-// Export error handlers for use in tests
-export { errorHandlers };
